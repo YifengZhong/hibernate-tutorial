c4db4566122d4c620d92635e8738b68fdcb1cda5
YifengZhong <yf_zhong@hotmail.com>
2019-12-10 23:31:34 -0600

    jdbc hibernate lib are added

---
+++ src/com/study/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package com.study.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		try {
+			System.out.println("connecting to database:"+ jdbcUrl);
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl,user,pass);
+			System.out.println("Connection successful");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
