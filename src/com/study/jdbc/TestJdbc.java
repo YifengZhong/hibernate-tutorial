84396887e5e864250539f89e297b1028c8ed431b
YifengZhong <yf_zhong@hotmail.com>
2019-12-11 21:55:09 -0600

    add test connection file

--- src/com/study/jdbc/TestJdbc.java
+++ src/com/study/jdbc/TestJdbc.java
@@ -8,7 +8,8 @@ public class TestJdbc {
 	public static void main(String[] args) {
 		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 		String user = "hbstudent";
-		String pass = "hbstudent";
+		String pass = "hbstudent"
+				;
 		try {
 			System.out.println("connecting to database:"+ jdbcUrl);
 			Connection myConn =
